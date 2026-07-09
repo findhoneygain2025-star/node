4b4c7ae68afafc370167e8932b3c940e3fe109c0
Honey <findhoneygain2025@gmail.com>
2026-07-09 00:36:46 -0700

    adding .evn file

--- backend/config/cloudinaryConfig.js
+++ backend/config/cloudinaryConfig.js
@@ -3,11 +3,10 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 require('dotenv').config();
 
-
 cloudinary.config({
-  cloud_name:'h0bwkdhy' ,
-  api_key: '615637364183765' ,
-  api_secret: 'l8dKy53FeXCwLCp2GVICa3Phb3s'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
@@ -20,4 +19,4 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({ storage: storage });
 
-module.exports = {upload,cloudinary};
+module.exports = { upload, cloudinary };
